05cdc232a0f3a36b2ecb35758729545002519e4d
Gubina Irina <irina.gubina98@mail.ru>
2020-09-08 17:51:29 +0300

    Изменения в src/

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	int Balance = 250;
-	int ReplenishmentAmount = 2780;
-	if (ReplenishmentAmount > 1_000) {
-	    int Result = Balance + ReplenishmentAmount + ReplenishmentAmount / 100;
-        System.out.println(Result);
-    } else {
-	    int Result = Balance + ReplenishmentAmount;
-        System.out.println(Result);
+	int balance = 250;
+	int replenishmentAmount = 2780;
+	int bonus;
+	if (replenishmentAmount > 1_000) {
+    bonus = replenishmentAmount / 100;
+	} else {
+	    bonus = 0;
     }
+    System.out.println(bonus);
     }
 }
